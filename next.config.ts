4745ac800b3d6a67b1de8c32f2b3b77e0caaa41f
Roksana Dilshad <roksanadilshad@gmail.com>
2026-01-15 17:19:53 +0600

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ["i.pinimg.com", "lh3.googleusercontent.com"], // add any other domains here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // This allows images from any HTTPS site (useful for demo/testing)
+      },
+      // OR be specific for security (Recommended):
+      // {
+        // protocol: 'https',
+        // hostname: 'res.cloudinary.com',
+        // port: '',
+        // pathname: '/**',
+      // },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
